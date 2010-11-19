a9d65e4babefe6ba3daa268b7e2949518aa81821
Romain <68727@supinfo.com>
2010-11-19 13:31:59 +0100

    Commit initial
    Signed-off-by: Romain <68727@supinfo.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupinfoTweetApp::Application.config.secret_token = '5066d38491ec0d10aaa78261bae05aa18dd1bde585200b7e9dd6c362bbecc149aa009b7dc8bfc594de3b488efa841274177032cc6a0a6a2b5667a0fff28710d6'
